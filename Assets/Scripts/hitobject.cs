59e17e97f3d582c6c43a868defd8482197ee0e18
christorres10 <47153998+christorres10@users.noreply.github.com>
2019-02-19 00:36:23 -0700

    Add files via upload

---
+++ Assets/Scripts/hitobject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class hitobject : MonoBehaviour
+{
+
+
+   void Update()
+    {
+        void OnCollisionEnter(Collision c)
+        {
+            float force = 3;
+            if (c.gameObject.name == "colorfulastroid")
+            {
+                Debug.Log("InCollision");
+                Vector3 dir = c.contacts[0].point - transform.position;
+                dir = -dir.normalized;
+                GetComponent<Rigidbody>().AddForce(dir * force);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/moveplayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveplayer : MonoBehaviour
+{
+    public float inputSpeed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+            transform.Translate
+            (-inputSpeed
+            * Time.deltaTime
+            , 0.0f, 0.0f);
+        if (Input.GetKey("up"))
+            transform.Translate(0.0f, inputSpeed* Time.deltaTime, 0.0f);
+        if (Input.GetKey("down"))
+            transform.Translate(0.0f, -inputSpeed* Time.deltaTime, 0.0f);
+    }
+}
